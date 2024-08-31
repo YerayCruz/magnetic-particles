5dc9223a0b03579678829b84e946e10e1ec4d3a1
YerayCruz <haplesshealer@outlook.com>
2024-08-30 18:17:25 -0600

    Feature: Initial position bonds

---
+++ src/initial_pos/init_pos.jl
@@ -0,0 +1,75 @@
+module BONDS_POSITION
+
+using Plots
+
+export initial_positions_distribution
+
+struct Chain
+    n::Int
+    position::Array{Float64, 3}
+end
+
+chains = []
+
+#At the end we have an array called chains with structures
+
+function initial_positions_distribution(sigma, L, particles_per_chain, nd, t)
+
+    for i = 1:length(particles_per_chain)
+
+        n = particles_per_chain[i]
+        zeros_array = zeros(Float64, length(t), n, nd)
+    
+        push!(chains, Chain(n, zeros_array))
+    
+    end
+    
+    number_bonds = length(chains)
+
+    bond_spacing = 2.5 * sigma
+    particle_spacing = 2.5 * sigma
+
+    bond_delim = number_bonds / 2
+    y_o = -bond_spacing * floor(bond_delim)
+
+    if length(chains) * bond_spacing > 2*L - 5*sigma
+        println("Error, bonds out of bounds")
+        return
+    end
+
+    for i = 1 : number_bonds
+
+        if chains[i].n * particle_spacing > 2*L - 5*sigma
+            println("Error, particles out of bounds")
+            return
+        end
+
+        particle_delim = chains[i].n / 2
+        x_o = -particle_spacing * floor(particle_delim)
+
+        for j = 1:chains[i].n
+            
+            chains[i].position[1, j, :] = [x_o, y_o]
+
+            x_o += particle_spacing
+
+        end
+
+        y_o += bond_spacing
+
+    end
+
+    plot(xlims = (-L, L), ylims = (-L, L))
+    for i = 1:number_bonds
+    
+        scatter!([chains[i].position[1, :, 1]], [chains[i].position[1, :, 2]])
+
+    end
+
+    savefig("positions.png")
+
+    return chains
+
+end
+
+end
--- src/main.jl
+++ src/main.jl
@@ -5,8 +5,14 @@ using Statistics
 
 using ProgressMeter
 
+include("./initial_pos/init_pos.jl")
+
+using .BONDS_POSITION
+
 k = 0.1
-d = 5
+d = 3.0
+
+bonds = [2, 2, 4]
 
 viscocity = 0.7
 
@@ -23,40 +29,35 @@ m = 1.0 # Mass of particles
 L = wall_dimension/2.0
 
 t = range(0, stop=40, length=4001)
-x = zeros(Float64, length(t), np, 2)
-v = zeros(Float64, length(t), np, 2)
-F = zeros(Float64, length(t), np, 2)
-temperature = zeros(Float64, length(t)) 
-
-
-# Generate unique initial conditions
-function initial_positions_distribution()
-    p_dist = 1.5*σ
-    x = zeros(Float64, np, 2)
-    first_x = -L + p_dist
-    first_y = -L + p_dist
-    x[1,1] = first_x
-    x[1,2] = first_y
-    for i=2:np
-        if first_x < L - p_dist
-            first_x += p_dist
-            x[i,1] = first_x
-            x[i,2] = first_y
-        else
-            first_x = -L + p_dist
-            first_y += p_dist
-            if first_y > L - p_dist
-                println("No more space in system")
-                return x[1:i-1, :] # Return already filled positions if out of space
-            end
-            x[i,1] = first_x
-            x[i,2] = first_y
-        end
-    end
-    return x
+struct Velocities
+  n::Int
+  velocity::Array{Float64, 3}
 end
 
-x[1,:,:] = initial_positions_distribution()
+struct Forces
+  n::Int
+  force::Array{Float64, 3}
+end
+
+v = []
+F = []
+
+for i = 1:length(bonds)
+  n = bonds[i]
+  zeros_array = zeros(Float64, length(t), n, 2)
+
+  push!(v, Velocities(n, zeros_array))
+end
+
+for i = 1:length(bonds)
+  n = bonds[i]
+  zeros_array = zeros(Float64, length(t), n, 2)
+
+  push!(F, Forces(n, zeros_array))
+end
+temperature = zeros(Float64, length(t)) 
+
+x = initial_positions_distribution(σ, L, bonds, 2, t) ## x es un array de n estructuras que contiene el numero de atomos de cada bond y sus posiciones
 
 function elastic_f(r, k, d)
     r_norm = sqrt(sum(r .^ 2))
@@ -73,36 +74,6 @@ function F_lj_2d(r, ϵ, σ)
 end
 
 #Update force calculation to handle boundaries and particle interactions
-function update_forces(x, F, i, ϵ, σ, np, viscocity, velocity)
-    j = 2
-
-    for ip = 1:np - 1
-        
-        FW_x1        = F_lj_2d([x[i, ip, 1].-(-L), 0], ϵ, σ)[1]
-        FW_x2        = F_lj_2d([x[i, ip, 1].-(+L), 0], ϵ, σ)[1]
-        FW_y1        = F_lj_2d([0, x[i, ip, 2].-(-L)], ϵ, σ)[2]
-        FW_y2        = F_lj_2d([0, x[i, ip, 2].-(+L)], ϵ, σ)[2]
-        FW           = [FW_x1 + FW_x2, FW_y1 + FW_y2]
-        F[i,ip, 1]   += FW[1]
-        F[i,ip, 2]   += FW[2]
-
-        FW_x1        = F_lj_2d([x[i, j, 1].-(-L), 0], ϵ, σ)[1]
-        FW_x2        = F_lj_2d([x[i, j, 1].-(+L), 0], ϵ, σ)[1]
-        FW_y1        = F_lj_2d([0, x[i, j, 2].-(-L)], ϵ, σ)[2]
-        FW_y2        = F_lj_2d([0, x[i, j, 2].-(+L)], ϵ, σ)[2]
-        FW           = [FW_x1 + FW_x2, FW_y1 + FW_y2]
-        F[i,j, 1]   += FW[1]
-        F[i,j, 2]   += FW[2]
-
-        for jp=j:np
-            r = x[i, ip, :] - x[i, jp, :]
-            force = elastic_f(r, k, d)
-            F[i, ip, :] += force - viscocity*velocity[ip, :]
-            F[i, jp, :] -= force + viscocity*velocity[jp, :]
-        end
-        j += 1
-    end
-end
 
 function wall_force(x, F, i, ϵ, σ, np, viscocity, velocity)
 
@@ -132,74 +103,45 @@ function wall_force(x, F, i, ϵ, σ, np, viscocity, velocity)
 
 end
 
-function bond_pos(p_dist, σ, L)
+function create_bond(x, viscocity, velocity, i, b) #x is an array
 
-    L *= 2
-    x_pos = 0, y_pos = 0 
-    p_spacing = 2.5*σ
-    if np == sum(p_dist)
-        x = zeros(Float64, np, 2)
-    end
+   for p = 1:x[b].n - 1
 
-    if length(p_dist) < L - 2*p_spacing
-        println("Unable to compute bonds, spcae to small")
-    end
-    for i = 1:length(p_dist)
-        if p_dist[i]*p_spacing < L - 2*p_spacing
-
-            b_distribution = [floor(p_dist[i]/2), p_dist[i] - floor(p_dist[i]/2)] #This part gives left part and right part from center of box, first particle of right side will always be in center.
-
-            l_counter = b_distribution[1], r_counter = b_distribution[2]
-
-            if l_counter > 0
-                for 1:l_counter
-
-                end
-            end
-
-
-        else
-            println("Unable to add bond $i")
-        end
-    end
-
-end
-
-
-function create_bond(p_dist, viscocity, velocity) #p_dist is an array that contains the number of particles per bond
-
-    
-    j = 2
-
-    for ip = 1:np - 1
-
-        for jp=j:np
-            r = x[i, ip, :] - x[i, jp, :]
-            force = elastic_f(r, k, d)
-            F[i, ip, :] += force - viscocity*velocity[ip, :]
-            F[i, jp, :] -= force + viscocity*velocity[jp, :]
-        end
-        j += 1
-    end
+     r = x[b].position[i, p, :] - x[b].position[i, p+1, :]
+     elastic_force = elastic_f(r, k, d)
+     F[b].force[i, p + 1, :] -= (elastic_force + viscocity * v[b].velocity[i, p + 1, :])
+     F[b].force[i, p, : ] += (elastic_force - viscocity * v[b].velocity[i, p, :])
+   end
+ 
 end
 
 Δt = step(t)
 @showprogress "Computing velocity-verlet" for i in 1:length(t)-1
-    
-    update_forces(x, F, i, ϵ, σ, np, viscocity, v[i, :, :])
-    v_mid = v[i,:,:] .+ Δt / 2 / m .* F[i,:,:]
-    x[i+1,:,:] = x[i,:,:] .+ Δt .* v_mid
-    update_forces(x, F, i+1, ϵ, σ, np, viscocity, v[i, :, :]) # Update forces based on new positions
-    v[i+1,:,:] = v_mid .+ Δt / 2 / m .* F[i+1,:,:]
+  for b = 1:length(bonds)
+
+    create_bond(x, viscocity, v, i, b)
+    v_mid = v[b].velocity[i, :, :] .+ Δt / 2 / m .* F[b].force[i, :, :]
+    x[b].position[i + 1, :, :] = x[b].position[i, :, :] .+ v_mid
+    create_bond(x, viscocity, v, i+1, b)
+    v[b].velocity[i+1, :, : ] = v_mid .+ Δt / 2 / m .* F[b].force[i+1, :, :]
+
+  end
+#    update_forces(x, F, i, ϵ, σ, np, viscocity, v[i, :, :])
+#    v_mid = v[i,:,:] .+ Δt / 2 / m .* F[i,:,:]
+#    x[i+1,:,:] = x[i,:,:] .+ Δt .* v_mid
+#    update_forces(x, F, i+1, ϵ, σ, np, viscocity, v[i, :, :]) # Update forces based on new positions
+#    v[i+1,:,:] = v_mid .+ Δt / 2 / m .* F[i+1,:,:]
 
 end
 
 
 anim = @animate for i = 1:5:length(t)
-    plot(legend=false, xlims=(-L, L), ylims=(-L, L), xlabel="x", ylabel="y", title="Time: $(round(t[i], digits=2))", xticks=-10:1:10)
-    for j = 1:np
-        scatter!([x[i,j,1]], [x[i,j,2]], markersize= σ*10)
-    end
+  
+  plot(legend=true, xlims=(-L, L), ylims=(-L, L), xlabel="x", ylabel="y", title="Time: $(round(t[i], digits=2))", xticks=-10:1:10)
+  for b = 1:length(bonds)
+
+    scatter!(x[b].position[i, :, 1], x[b].position[i, :, 2], markersize= σ*10)
+  end
 end
 
 gif(anim, "particle_simulation.gif", fps = 24)
